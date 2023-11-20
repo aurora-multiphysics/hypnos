1d6c1a71fccd3965b47cfa753a3586c84bcfccd7
sid-mungale <sid.mungale@ukaea.com>
2023-11-20 13:19:54 +0000

    rewrite complex component as collection of volumes

--- cubism.py
+++ cubism.py
@@ -34,6 +34,7 @@ BLANKET_ALL = BLANKET_REQUIREMENTS + BLANKET_ADDITIONAL
 # classes according to what make_geometry subfunction(?) needs to be called
 BLOB_CLASSES = ["complex", "breeder", "structure", "air"]
 ROOM_CLASSES = ["room"]
+WALL_CLASSES = ["wall"]
 
 # currently only supports exclusive, inclusive, and overlap
 FACILITY_MORPHOLOGIES= ["exclusive", "inclusive", "overlap", "wall"]
@@ -177,6 +178,10 @@ class GenericComponentAssembly:
                 elif isinstance(component, GenericComponentAssembly):
                     instances_list += component.get_all_generic_cubit_instances()
         return instances_list
+
+    def get_volumes_list(self) -> list:
+        volumes_list = from_bodies_to_volumes(self.get_all_generic_cubit_instances)
+        return [volume.cid for volume in volumes_list]
 
 class CreatedComponentAssembly(GenericComponentAssembly):
     '''
@@ -390,7 +395,7 @@ class CreatedCubitInstance(GenericCubitInstance):
             return self.__create_cubit_blob(geometry)
         elif self.classname in ROOM_CLASSES:
             return self.__create_cubit_room(geometry)
-        elif self.classname == "wall":
+        elif self.classname in WALL_CLASSES:
             return self.__create_cubit_wall(geometry)
         else:
             raise CubismError("Wrong class name somewhere?: " + self.classname)
@@ -685,14 +690,126 @@ class MaterialsTracker:
             cubit.cmd(f'sideset {boundary.group_id} name "{boundary.name}"')
 
 # very basic implementations for component classes created natively
-class ComplexComponent(CreatedCubitInstance):
+class ComplexComponent:
     # stores information about what materials exist. geometries can then be found from groups with the same name
     complexComponentMaterials = MaterialsTracker()
     def __init__(self, geometry, classname, material):
-        CreatedCubitInstance.__init__(self= self, geometry= geometry, classname= classname)
+        self.subcomponents = []
+        self.classname = classname
+        self.geometry = geometry
         self.material = material
+        self.make_geometry()
         # add geometry to material tracker
-        self.complexComponentMaterials.add_geometry_to_material(GenericCubitInstance(self.cid, self.geometry_type), self.material)
+        for subcomponent in self.subcomponents:
+            self.complexComponentMaterials.add_geometry_to_material(subcomponent, self.material)
+    
+    def add_to_subcomponents(self, subcomponents):
+        '''Add GenericCubitInstance or list of GenericCubitInstances to subcomponents attribute'''
+        if isinstance(subcomponents, GenericCubitInstance):
+            self.subcomponents.append(subcomponents)
+        elif type(subcomponents) == list:
+            for subcomponent in subcomponents:
+                if isinstance(subcomponent, GenericCubitInstance):
+                    self.subcomponents.append(subcomponents)
+
+    def make_geometry(self):
+        '''create geometry in cubit. if the class is a blob, make a blob. if the class is a room, make a room. otherwise break.'''
+        if self.classname in BLOB_CLASSES:
+            self.add_to_subcomponents(self.__create_cubit_blob(self.geometry))
+        elif self.classname in ROOM_CLASSES:
+            self.add_to_subcomponents(self.__create_cubit_room(self.geometry))
+        elif self.classname in WALL_CLASSES:
+            self.add_to_subcomponents(self.__create_cubit_wall(self.geometry))
+        else:
+            raise CubismError("Wrong class name somewhere?: " + self.classname)
+
+    def __convert_to_3d_vector(self, dim):
+        if type(dim) == int:
+            return_vector = [dim for i in range(3)]
+        elif len(dim) == 1:
+            return_vector = [dim[0] for i in range(3)]
+        elif len(dim) == 3:
+            return_vector = dim
+        else:
+            raise CubismError("thickness should be either a 1D or 3D vector (or scalar)")
+        return return_vector
+
+    def __create_cubit_blob(self, geometry: dict):
+        '''create cube (if scalar/1D) or cuboid (if 3D) with dimensions. 
+        Rotate it about the y-axis, x-axis, y-axis if euler_angles are specified. 
+        Move it to position if specified'''
+        # setup variables
+        dims= self.__convert_to_3d_vector(geometry["dimensions"])
+        pos= geometry["position"] if "position" in geometry.keys() else [0, 0, 0]
+        euler_angles= geometry["euler_angles"] if "euler_angles" in geometry.keys() else [0, 0, 0]
+        # create a cube or cuboid.
+        blob = cubit.brick(dims[0], dims[1], dims[2])
+        cid = cubit.get_last_id("volume")
+        # orientate according to euler angles
+        axis_list = ['y', 'x', 'y']
+        for i in range(3): # hard-coding in 3D?
+            if not euler_angles[i] == 0:
+                cubit.cmd(f'rotate volume {cid} angle {euler_angles[i]} about {axis_list[i]}')
+        # move to specified position
+        cubit.move(blob, pos)
+        # return instance for further manipulation
+        return GenericCubitInstance(cid, "volume")
+
+    def __create_cubit_room(self, geometry: dict):
+        '''create 3d room with outer dimensions dimensions (int or list) and thickness (int or list)'''
+        # get variables
+        outer_dims= self.__convert_to_3d_vector(geometry["dimensions"])
+        thickness= self.__convert_to_3d_vector(geometry["thickness"])
+        # create room
+        subtract_vol = cubit.brick(outer_dims[0]-2*thickness[0], outer_dims[1]-2*thickness[1], outer_dims[2]-2*thickness[2])
+        block = cubit.brick(outer_dims[0], outer_dims[1], outer_dims[2])
+        room = cubit.subtract([subtract_vol], [block])
+        room_id = cubit.get_last_id("volume")
+        return GenericCubitInstance(room_id, "volume")
+
+    def __create_cubit_wall(self, geometry: dict):
+        # get variables
+        # wall
+        thickness= geometry["wall thickness"]
+        plane= geometry["wall plane"] if "wall plane" in geometry.keys() else "x"
+        pos= geometry["wall position"] if "wall position" in geometry.keys() else 0
+        # hole
+        hole_pos= geometry["wall hole position"] if "wall hole position" in geometry.keys() else [0, 0]
+        hole_radius= geometry["wall hole radius"]
+        # wall fills room
+        room_dims= self.__convert_to_3d_vector(geometry["dimensions"])
+        room_thickness= self.__convert_to_3d_vector(geometry["thickness"])
+        wall_dims = [room_dims[i]-2*room_thickness[i] for i in range(3)]
+
+        # volume to subtract to create a hole
+        cubit.cmd(f"create cylinder height {thickness} radius {hole_radius}")
+        subtract_vol = GenericCubitInstance(cubit.get_last_id("volume"), "volume")
+
+        # depending on what plane the wall needs to be in, create wall + make hole at right place + move wall
+        if plane == "x":
+            cubit.brick(thickness, wall_dims[1], wall_dims[2])
+            wall = GenericCubitInstance(cubit.get_last_id("volume"), "volume")
+            cubit.cmd(f"rotate volume {subtract_vol.cid} angle 90 about Y")
+            cubit.cmd(f"move volume {subtract_vol.cid} y {hole_pos[1]} z {hole_pos[0]}")
+            cubit.cmd(f"subtract volume {subtract_vol.cid} from volume {wall.cid}")
+            cubit.cmd(f"move volume {wall.cid} x {pos}")
+        elif plane == "y":
+            cubit.brick( wall_dims[0], thickness, wall_dims[2])
+            wall = GenericCubitInstance(cubit.get_last_id("volume"), "volume")
+            cubit.cmd(f"rotate volume {subtract_vol.cid} angle 90 about X")
+            cubit.cmd(f"move volume {subtract_vol.cid} x {hole_pos[0]} z {hole_pos[1]}")
+            cubit.cmd(f"subtract volume {subtract_vol.cid} from volume {wall.cid}")
+            cubit.cmd(f"move volume {wall.cid} y {pos}")
+        elif plane == "z":
+            cubit.brick( wall_dims[0], wall_dims[1], thickness)
+            wall = GenericCubitInstance(cubit.get_last_id("volume"), "volume")
+            cubit.cmd(f"move volume {subtract_vol.cid} x {hole_pos[0]} y {hole_pos[1]}")
+            cubit.cmd(f"subtract volume {subtract_vol.cid} from volume {wall.cid}")
+            cubit.cmd(f"move volume {wall.cid} z {pos}")
+        else:
+            raise CubismError("unrecognised plane specified")
+        
+        return GenericCubitInstance(wall.cid, wall.geometry_type)            
     
     def update_reference_and_tracking(self, cid, geometry_type):
         '''Change what geometry this instance refers to'''
