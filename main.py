335aad8c006232d0a7265d730c63bd8f65d1ddcc
sid-mungale <sid.mungale@ukaea.com>
2024-01-24 09:25:54 +0000

    migrate out direct cubit references from main

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-#import json
 import argparse
-#from constants import CLASS_MAPPING
-from components import *
 from assemblies import *
-from cubit_functions import extract_data
+from cubit_functions import extract_data, initialise_cubit, reset_cubit
 
 class ComponentTracker:
     '''Adds components to cubit groups recursively'''
@@ -82,20 +78,16 @@ def read_file(filename):
     raise CubismError("File not in readable format")
 
 if __name__ == '__coreformcubit__':
-    cubit.cmd("reset")
+    reset_cubit()
     read_file("sample_morphology.json")
 elif __name__ == "__main__":
     # accept command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", type=str, help="name of json file describing geometry", default="sample_blanket_ring.json")
-    parser.add_argument("-c", "--cubitpath", type=str, help="path of the cubit python library", default='/opt/Coreform-Cubit-2023.8/bin')
     parser.add_argument("-i", "--info", action="store_true")
     args = parser.parse_args()
 
-    # if this is run as a python file, import cubit
-    sys.path.append(args.cubitpath)
-    import cubit
-    cubit.init(['cubit', '-nojournal'])
+    initialise_cubit()
 
     universe = read_file(args.file)
     # track all components, materials, and boundaries as groups
