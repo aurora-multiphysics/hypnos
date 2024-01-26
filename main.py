23826051cc38872ccc6d293770138bcd73b00981
sid-mungale <sid.mungale@ukaea.com>
2024-01-26 11:35:44 +0000

    restructure read_file

--- main.py
+++ main.py
@@ -2,38 +2,40 @@ import argparse
 from component_tracker import ComponentTracker
 from assemblies import *
 from cubit_functions import initialise_cubit, reset_cubit
-from parsing import extract_data
+from parsing import extract_data, ParameterFiller
 
-def read_file(filename):
-    '''Read in json file, construct all specified components
+def make_everything(json_object):
+    '''Construct all specified components
 
     :return: list of all top-level components
     :rtype: list
     '''
-    objects = extract_data(filename)
     universe = []
-    if type(objects) == list:
-        for json_object in objects:
-            universe.append(construct(json_object))
+    if type(json_object) == list:
+        for json_component in json_object:
+            universe.append(construct(json_component))
         return universe
-    elif type(objects) == dict:
-        universe.append(construct(objects))
+    elif type(json_object) == dict:
+        universe.append(construct(json_object))
         return universe
-    raise CubismError("File not in readable format")
+    raise CubismError("json object not recognised")
 
 if __name__ == '__coreformcubit__':
     reset_cubit()
-    read_file("sample_morphology.json")
+    make_everything(extract_data("sample_morphology.json"))
 elif __name__ == "__main__":
     # accept command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", type=str, help="name of json file describing geometry", default="sample_blanket_ring.json")
+    parser.add_argument("-f", "--file", type=str, help="name of json file describing geometry", default="sample_blanket.json")
     parser.add_argument("-i", "--info", action="store_true")
     args = parser.parse_args()
 
     initialise_cubit()
 
-    universe = read_file(args.file)
+    json_object = extract_data(args.file)
+    filled_json_object = ParameterFiller().process_defaults(json_object)
+    ParameterFiller().print_log()
+    universe = make_everything(filled_json_object)
     # track all components, materials, and boundaries as groups
     for component in universe:
         print(f"components being tracked in root {ComponentTracker(component).root_name}")
