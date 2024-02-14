2ac6d7b1e2003e3581d5f75ec7ac449745c191a9
sid-mungale <sid.mungale@ukaea.com>
2024-02-14 16:50:22 +0000

    add hcpb default config

--- constants.py
+++ constants.py
@@ -2,7 +2,8 @@
 NEUTRON_TEST_FACILITY_REQUIREMENTS = ["room", "source"]
 BLANKET_REQUIREMENTS = ["breeder", "structure"]
 ROOM_REQUIREMENTS = ["blanket", "surrounding_walls"]
-HCPB_BLANKET_REQUIREMENTS = ["first_wall", "breeder_unit"]
+BLANKET_SHELL_REQUIREMENTS = ["first_wall", "breeder_unit"]
+HCPB_BLANKET_REQUIREMENTS = ["first_wall", "breeder_unit", "front_rib", "back_rib", "coolant_outlet_plenum"]
 
 # classes according to what make_geometry subfunction(?) needs to be called
 BLOB_CLASSES = ["complex", "breeder", "structure", "air"]
