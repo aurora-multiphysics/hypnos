c8de20b66336a271fc1b62c9a4dd54538908804f
sid-mungale <sid.mungale@ukaea.com>
2024-07-02 16:38:40 +0100

    add many tests

--- tests/test_generic_classes.py
+++ tests/test_generic_classes.py
@@ -1,16 +1,23 @@
 import cubit
 import pytest
+import numpy as np
 from blobmaker.generic_classes import (
     CubitInstance,
-    get_cubit_geometry
+    get_cubit_geometry,
+    cmd,
+    CubismError
 )
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def brick():
     cubit.reset()
     cubit.brick(1, 1, 1)
     return CubitInstance(1, "body")
 
+def test_cmd():
+    assert cubit.get_entities("volume") == ()
+    cmd("create brick x 5")
+    assert list(cubit.get_entities("volume")) == [1]
 
 class TestCubitInstance:
     
@@ -45,3 +52,25 @@ def test_get_cubit_geometry(brick):
     brick_handle = get_cubit_geometry(1, "volume")
     cubit.move(brick_handle, [1, 0, 0])
     assert brick_handle.centroid() == (1, 0, 0)
+
+    cubit.reset()
+    cmd("create vertex 1 1 1")
+    vertex_handle = get_cubit_geometry(1, "vertex")
+    assert vertex_handle.coordinates() == (1, 1, 1)
+
+    cubit.reset()
+    cmd("create vertex 1 1 1")
+    cmd("create vertex 0 0 0")
+    cmd("create curve vertex 1 2")
+    curve_handle = get_cubit_geometry(1, "curve")
+    assert curve_handle.curve_center() == (0.5, 0.5, 0.5)
+
+    cubit.reset()
+    cmd("create surface circle radius 5")
+    surf_handle = get_cubit_geometry(1, "surface")
+    assert surf_handle.area() == pytest.approx(25*np.pi)
+
+def test_cubism_error():
+    with pytest.raises(CubismError) as excinfo:
+        raise CubismError("error occurred")
+    assert excinfo.match("error occurred")
