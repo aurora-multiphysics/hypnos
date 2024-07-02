034a8d6a0b16926c9c57cb0e3c52e5dafc1d1dda
sid-mungale <sid.mungale@ukaea.com>
2024-07-02 16:38:40 +0100

    add many tests

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -8,8 +8,10 @@ from blobmaker.geometry import (
     hypotenuse,
     arctan,
     Vertex,
-    make_surface
+    make_surface,
+    Line
 )
+from blobmaker.generic_classes import CubitInstance
 import pytest, cubit
 import numpy as np
 
@@ -34,7 +36,13 @@ def midpoints():
 
 @pytest.fixture()
 def vertex():
+    '''1 2 3'''
     return Vertex(1, 2, 3)
+
+@pytest.fixture()
+def line():
+    '''y = 2x - 2'''
+    return Line(Vertex(1, 2), Vertex(1))
 
 
 def test_create_2d_vertex():
@@ -45,6 +53,8 @@ def test_create_2d_vertex():
 
 def test_connect_vertices_straight(verts):
     connecting_curve = connect_vertices_straight(verts[0], verts[1])
+    assert isinstance(connecting_curve, CubitInstance)
+    assert connecting_curve.cid > 0
     assert connecting_curve.handle.curve_center() == (0, -5, 0)
 
 def test_connect_curves_tangentially(verts):
@@ -87,13 +97,21 @@ def test_arctan():
     assert arctan(1, 0) == np.pi/2
     assert arctan(3, -3) == 3*np.pi/4
 
+
 class TestVertex:
+    def test_eq(self, vertex):
+        assert vertex == Vertex(1, 2, 3)
+        assert vertex != Vertex(1, 2, 2)
+    
+    def test_repr(self, vertex):
+        assert vertex == eval(repr(vertex))
+
     def test_str(self, vertex):
         assert str(vertex) == "1 2 3"
 
     def test_add(self, vertex: Vertex):
         vert_sum = vertex + Vertex(1, 0, -1)
-        assert str(vert_sum) == "2 2 2"
+        assert vert_sum == Vertex(2, 2, 2)
 
     def test_create(self, vertex: Vertex):
         created_vertex = vertex.create()
@@ -105,9 +123,34 @@ class TestVertex:
 
     def test_distance(self, vertex: Vertex):
         assert vertex.distance() == hypotenuse(vertex.x, vertex.y, vertex.z)
+    
+    def test_unit(self, vertex: Vertex):
+        assert Vertex(5, 0, 0).unit() == Vertex(1, 0, 0)
+        assert vertex.unit().distance() == 1
+    
+    def test_extend_to_y(self):
+        assert Vertex(1, 1).extend_to_y(5).x == 5
+        assert Vertex(-3, 2).extend_to_y(-4).x == 6
+    
+    def test_extend_to_x(self):
+        assert Vertex(1, 1).extend_to_x(5).y == 5
+        assert Vertex(4, 5).extend_to_x(8).y == 10
 
 
-def test_make_surface(verts):
+class TestLine:
+    def test_eq(self, line):
+        assert line == Line(Vertex(1, 2), Vertex(1))
+    
+    def test_repr(self, line):
+        assert line == pytest.approx(eval(repr(line)))
+    
+    def test_vertex_at(self, line: Line):
+        assert line.vertex_at(0) == Vertex(0, -2)
+        assert line.vertex_at(0, 5, 2) == Vertex(0, -2)
+        assert line.vertex_at(y=2) == Vertex(2, 2)
+        assert line.vertex_at(z=3) == None
+
+def test_make_surface():
     vertices = [Vertex(5, 5), Vertex(5, -5), Vertex(-5, -5), Vertex(-5, 5)]
     surf = make_surface(vertices, []).handle
     assert surf.area() == 100
