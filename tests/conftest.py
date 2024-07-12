14231902036ad3dc63a1184eb56328f2a269e916
sid-mungale <sid.mungale@ukaea.com>
2024-07-12 13:37:58 +0100

    rescope fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,12 @@
 import pytest
 import cubit
-from blobmaker.components import SimpleComponent
 
+# initialise cubit once
 @pytest.fixture(scope="session", autouse=True)
 def initialise():
     return cubit.init([])
 
-@pytest.fixture(scope="module")
-def geometry_json():
-    return {
-        "material": "Steel",
-        "geometry": {
-            "dimensions": 5
-        },
-        "origin": [10, 0, 0]
-    }
-
-@pytest.fixture(scope="function")
-def simple_component(geometry_json):
-    cubit.reset()
-    return SimpleComponent("air", geometry_json)
-
+# cleanup after every test
 @pytest.fixture(autouse=True)
 def reset():
     yield 0
