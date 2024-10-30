4840eb0e93adbce031374abf8cc95cc21e692a6a
sid-mungale <sid.mungale@ukaea.com>
2024-10-30 14:11:24 +0000

    refactor test funcs

--- tests/test_construct.py
+++ tests/test_construct.py
@@ -1,7 +1,5 @@
 from blobmaker.default_params import FIRST_WALL, PIN, HCPB_BLANKET
-from blobmaker.cubit_functions import cmd, union
-from blobmaker.geometry_maker import GeometryMaker
-from blobmaker.generic_classes import CubitInstance
+from funcs_for_tests import get_union_volumes
 import cubit
 import pytest
 
@@ -50,47 +48,3 @@ def test_blanket(goldpath):
     gold_vol, maker_vol, net_vol = get_union_volumes(blanket_path, HCPB_BLANKET)
     appx = pytest.approx
     assert gold_vol == appx(maker_vol) == appx(net_vol)
-
-
-def get_union_volumes(goldfile: str, maker_tree: dict):
-    '''Get the volumes of the gold file geometry, geometry from a design tree,
-    and their union
-
-    :param goldfile: gold filepath
-    :type goldfile: str
-    :param maker_tree: design tree
-    :type maker_tree: dict
-    :return: volume values
-    :rtype: tuple[int]
-    '''
-
-    # instantiate maker in case cubit.init() changes anything
-    maker = GeometryMaker()
-
-    # import gold file, get its volumes
-    stray_vol_ids = vols()
-    cmd(f'import cubit "{goldfile}"')
-    post_import_vol_ids = vols()
-    gold_vol_ids = post_import_vol_ids.difference(stray_vol_ids)
-
-    # make geometry, get its volumes
-    maker.design_tree = maker_tree
-    maker.make_geometry()
-    maker_vol_ids = vols().difference(post_import_vol_ids)
-
-    # union of all gold volumes, all maker volumes
-    gold_union = union([CubitInstance(vol_id, "volume") for vol_id in list(gold_vol_ids)])
-    maker_union = union([CubitInstance(vol_id, "volume") for vol_id in list(maker_vol_ids)])
-
-    gold_volume = sum([vol.handle.volume() for vol in gold_union])
-    maker_volume = sum([vol.handle.volume() for vol in maker_union])
-
-    # union of above 2 unions
-    net_union = union(gold_union + maker_union)
-    net_volume = sum([vol.handle.volume() for vol in net_union])
-
-    return gold_volume, maker_volume, net_volume
-
-
-def vols():
-    return set(cubit.get_entities("volume"))
